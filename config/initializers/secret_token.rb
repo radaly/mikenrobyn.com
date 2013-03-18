0ea5a3491f8c8498f25ad0cde83402bf07ceb6d3
Robyn Chadwick <radaly@gmail.com>
2013-03-17 21:00:35 -0700

    Added all the things

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mikenrobyn::Application.config.secret_token = '21b09667f3d6a93120172ded9d95f832b8287f119c8176651eb0ae864ab67197aebd5d1a0e07df9267b4d469e307592769153107457820996204a34908d56ea9'
